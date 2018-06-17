7ee95c192069169f95248aa92b1f7a2aafd1e699
fwend <josvanuden@gmail.com>
2018-06-17 10:33:44 +0200

    indent

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 let fibonacci = (function () {
 
-  const cache = { 0: 0, 1: 1 };
+    const cache = { 0: 0, 1: 1 };
 
-  return function (n) {
+    return function (n) {
 
-    if (!cache[n - 1]) {
-      cache[n - 1] = fibonacci(n - 1);
-    }
+        if (!cache[n - 1]) {
+            cache[n - 1] = fibonacci(n - 1);
+        }
 
-    return cache[n - 1] + cache[n - 2];
-  };
+        return cache[n - 1] + cache[n - 2];
+    };
 })();

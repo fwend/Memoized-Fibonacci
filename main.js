09fa0a41dc299553586378357c209035335b87f6
Jos <josvanuden@gmail.com>
2018-06-17 09:28:29 +0200

    initial version

---
+++ main.js
@@ -0,0 +1,13 @@
+var fibonacci = (function () {
+
+    const cache = {0 : 0, 1 : 1};
+    
+    return function(n) {
+      
+      if (!cache[n - 1]) {
+          cache[n - 1] = fibonacci(n - 1);
+      }
+      
+      return cache[n - 1] + cache[n - 2];
+    };
+  })();
